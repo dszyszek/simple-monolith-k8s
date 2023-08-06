724190dd7bd3d63ab081e9f79710acbfa3da43bd
Damian Szyszka <dszyszek@gmail.com>
2023-08-07 01:10:56 +0200

    Serve next static files properly; adjust consts regarding client code

--- web_server/src/server/server.ts
+++ web_server/src/server/server.ts
@@ -2,21 +2,23 @@ import express from "express";
 
 import type { ServerConfig } from "./types";
 import { api } from "../api";
-import { CLIENT_CODE_DIST_HTML_ROOT } from "./const";
+import { CLIENT_CODE_DIST_PATH } from "./const";
 import { setHeaders, isProduction } from "./utils";
 
 const runWebServer = ({ port }: ServerConfig) => {
   const server = express();
-  const clientCode = CLIENT_CODE_DIST_HTML_ROOT;
+  const clientRoot = CLIENT_CODE_DIST_PATH;
 
   server.use(setHeaders);
 
   server.use("/api/v1", api);
 
   if (isProduction()) {
-    server.get("*", (request, response) => {
-      response.sendFile(clientCode);
-    });
+    server.use(express.static(clientRoot));
+
+    // server.get("*", (request, response) => {
+    //   response.sendFile(clientHtmlRoot);
+    // });
   }
 
   server.listen(port, () => {
