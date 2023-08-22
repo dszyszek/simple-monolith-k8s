94bf280b74e387b733a7fb25da960a3caaaed1e4
Damian Szyszka <dszyszek@gmail.com>
2023-08-23 00:44:38 +0200

    Connect to db; Add UserElement entity; connect to db from server.ts & add bodyParser

---
+++ web_server/src/entity/index.ts
@@ -0,0 +1,1 @@
+export { default as UserElement } from "./UserElement";
--- web_server/src/server/server.ts
+++ web_server/src/server/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 import type { ServerConfig } from "./types";
 import { api } from "../api";
 import { setHeaders } from "./utils";
+import connectDB from "../db/connectDB";
 
 const runWebServer = ({ port }: ServerConfig) => {
   const server = express();
 
+  connectDB;
+
+  server.use(bodyParser.json());
   server.use(setHeaders);
 
   server.use("/api/v1", api);
